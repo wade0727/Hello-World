d7e78ad2d57cfafb7ebfadf823792eb51b04d23d
Matt <Matt@MATT-PC>
2016-04-28 00:29:23 -0500

    Add project files.

---
+++ Hello World/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = "Hello World";
+            string anotherVariable = "Hello Again";
+
+            int number1 = 5;
+            int number2 = 5;
+
+            string answer = number1.ToString() + number2.ToString();
+
+            Console.WriteLine(answer);
+
+            bool boolVar = false;
+                        
+            //Console.WriteLine(myValue);
+            //Console.WriteLine(anotherVariable);
+            //anotherVariable = myValue;
+            //Console.WriteLine(anotherVariable);
+        }
+    }
+}
